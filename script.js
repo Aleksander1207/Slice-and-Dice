ff00532d7969163f42c2dfb33837e55ca4111dc7
Aleksander <aleksander.aleksiev12@gmail.com>
2021-06-28 00:27:02 +0300

    Making more responsive design

--- script.js
+++ script.js
@@ -18,12 +18,22 @@ function carousel(){
 }
 
 function toggleNavigation(){
+    let title = document.getElementById('title');
+    let paragraph = document.getElementById('main-p');
     let navIcon = document.getElementById('nav-icon');
     let list = document.getElementsByTagName('ul');
     if(list[0].style.display == 'none'){
         list[0].style.display = 'block';
+        title.style.color = 'transparent';
+        paragraph.style.color = 'transparent';
+        title.style.textShadow = '0 0 8px #000';
+        paragraph.style.textShadow = '0 0 8px #000';
     }
     else{
         list[0].style.display = 'none';
+        title.style.color = 'white';
+        paragraph.style.color = 'white';
+        title.style.textShadow = 'none';
+        paragraph.style.textShadow = 'none';
     }
 }
